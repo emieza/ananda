3f1a521dfb45f7f2136902004e06607bd3d7665c
Enric Mieza <emieza@xtec.cat>
2016-05-10 23:57:12 +0200

    readme file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# ananda
+# Ananda
+
+Ananda is a simple CMS for webpages using Django framework.
+
+## Installation
+
+- Clone the project from GitHub
+- Create database with
+    python manage.py migrate
+- Collect static files with
+    python manage.py collectstatic
+- Run server with or install in webserver using WSGI
+    python manage.py runserver
+
+
--- web/models.py
+++ web/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from datetime import datetime
 from django.db import models
 from ckeditor.fields import RichTextField
+from django.utils.timezone import now
 
 # Create your models here.
 
@@ -9,7 +10,7 @@ class Page(models.Model):
 	name = models.CharField(max_length=200)
 	active = models.BooleanField(default=True)
 	menu = models.BooleanField(default=True)
-	creation_date = models.DateTimeField(default=datetime.now())
+	creation_date = models.DateTimeField(default=now())
 	modified_date = models.DateTimeField()
 	order = models.IntegerField(default=0)
 	text = RichTextField()
